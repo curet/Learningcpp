da49323bb28b216f89204349836bf13903b6097b
curet <45988719+curet@users.noreply.github.com>
2020-01-02 20:59:28 -0400

    Update 1_Introduction.cpp
    Variables and literals
    Identifiers
    Integers Data Types

--- Chapters/1_Introduction.cpp
+++ Chapters/1_Introduction.cpp
@@ -34,11 +34,108 @@ cout << endl << "cat" << endl << "bird" << endl;
 using namespace std;
 int main()
 {
-	cout << "Sucess\n";
-	cout << "Sucess Sucess\n\n";
-	cout << "Sucess";
+	cout << "Success\n";
+	cout << "Success Success\n\n";
+	cout << "Success";
 
 return 0;
 }
+
+//--------------------------------------------------------------------------
+// VARIABLES AND LITERALS
+
+// Example
+int number
+
+//--------------------------------------------------------------------------
+
+// This program has a variable.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number;
+
+	number = 5;
+	cout << "The value in number is " << number << endl;
+	return 0;
+}
+
+//--------------------------------------------------------------------------
+
+number = 5; // This is called an assignment,
+
+// The equal sign is an operator that copies the value
+// on it's right (5) into the variable named on its left
+// (number). 
+
+// After this line executes, number will be set to 5.
+
+//--------------------------------------------------------------------------
+// 2.5 IDENTIFIERS
+// Recommended variable declaration style
+int itemsOrdered; // Recommended
+int x;		  // Instead of this one
+
+//--------------------------------------------------------------------------
+// 2.6 INTEGER DATA TYPES
+// This program has variables of several
+// of the integer types.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int checking;
+	unsigned int miles;
+	long days;
+
+	checking = -20;
+	miles = 4276;
+	days = 189000;
+
+	cout << "We have made a long journey of " << miles;
+	cout << " miles.\n";
+
+	cout << "Out checking account balance is " << checking;
+	cout << "\nAbout " << days << " days ago Columbus ";
+	cout << "stood on this spot.\n";
+
+	return 0;
+}
+
+//--------------------------------------------------------------------------
+// Ways to define different variables of the same data type
+
+// Way 1
+	int length;
+	int width;
+
+// Way 2
+	int length, width;
+//--------------------------------------------------------------------------
+
+// force an integer literal to be stored as a long integer by placing the letter L at the end
+long amount;
+amount = 32L;
+
+// If I want an integer literal to be treated as
+// a long long int, I can append LL at the end of
+// the number.
+
+// Example:
+long long amount;
+amount = 32LL;
+
+//--------------------------------------------------------------------------
+// Hexadecimal number F4 expressed in C++
+// -->> 0xF4
+
+// Octal must be preceded by a 0.
+// Example octal 31 would be:
+// -->> 031
 
 //--------------------------------------------------------------------------
