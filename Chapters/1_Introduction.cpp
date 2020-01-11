fc9d3881a741a64f3863e6ab68fe90bd7361c770
Curet <45988719+curet@users.noreply.github.com>
2020-01-10 23:40:05 -0400

    2.11 Determining the Size of a Data Type

--- Chapters/1_Introduction.cpp
+++ Chapters/1_Introduction.cpp
@@ -318,5 +318,24 @@ int main()
 }
 
 //--------------------------------------------------------------------------
+2.11 DETERMINING THE SIZE OF A DATA TYPE
+// This program determine the size of integers, long,
+// integers, and long doubles.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long double apple;
+	
+	cout << "The size of an integer is " << sizeof(int);
+	cout << " bytes.\n";
+	cout << "The size of a long integer is " << sizeof(long);
+	cout << " bytes.\n";
+	cout << "An apple can be eaten in " << sizeof(apple);
+	cout << " bytes!\n";
+	return 0;
+
+}
 
 
