b789eca1bc46ca0585c35857d14b15f002e6198b
Curet <45988719+curet@users.noreply.github.com>
2020-01-12 22:48:03 -0400

    2.12 Variable Assignments and Initialization

--- Chapters/1_Introduction.cpp
+++ Chapters/1_Introduction.cpp
@@ -338,4 +338,15 @@ int main()
 
 }
 
-
+//--------------------------------------------------------------------------
+2.12 VARIABLE ASSIGNMENTS AND INITIALIZATION
+// This program shows variable initialization.
+#include <iostream>
+using namespace std;
+int main()
+{
+	int month = 2, days = 28;
+	
+	cout << "Month " << month << " has " << days << " days.\n";
+	return 0;
+}
