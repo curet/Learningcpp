11e6fdd98f19050ad582b67f4f33f10823f65d24
curet <jcscuret@gmail.com>
2020-02-05 19:25:45 -0400

    Create 1.5-Input-Procesing-and-Output.cpp

---
+++ Chapters/1.-Introduction-to-Computers-and-Programming/1.5-Input-Procesing-and-Output.cpp
@@ -0,0 +1,16 @@
+// INPUT PROCESSING AND OUTPUT
+
+// Input example
+
+cin >> hours;
+cin >> rate;
+
+// Processing example
+
+pay = hours * rate;
+
+// Output example
+
+cout << "How many hours did you work? ";
+cout << "How much do you get paid per hour? ";
+cout << "You have earned $" << pay << endl;
