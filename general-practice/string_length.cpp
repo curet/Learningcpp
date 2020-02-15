dcbb4fa593a1c3cd9a081cb29ba1b1da875895e6
curet <jcscuret@gmail.com>
2020-02-15 11:02:20 -0400

    Create string_length.cpp

---
+++ general-practice/string_length.cpp
@@ -0,0 +1,13 @@
+// Practicing with the .length() method
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string word;
+  cout << "Enter the word you are interested on knowing its length: ";
+  cin >> word;
+  cout << "The lenght of " << word << " is " << word.length() << endl;
+
+}
