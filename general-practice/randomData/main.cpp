77f6b19fd73ec8c5500c4d500bd5a3800e077d6b
curet <jcscuret@gmail.com>
2020-02-27 22:34:26 -0400

    Update main.cpp

--- general-practice/randomData/main.cpp
+++ general-practice/randomData/main.cpp
@@ -4,7 +4,7 @@
  *
  * Created by José Constantino Sánchez Curet
  *
- * Copyright 2020. All rights reserved
+ * Copyright 2020 José Constantino Sánchez Curet. All rights reserved
  *
  * Purpose: This program create a file from a 10x column size, then create the amount of rows as the user's prefer based
  * on the input validation.
@@ -22,7 +22,7 @@ int main() {
     // Variables Declaration
     double one, two, three, four, five, six, seven, eight, nine, ten;
     double sum = 0, max;
-    int row =1;
+    int row =1, rowsLimit;
     int mod = 6;
     ifstream inFile;
     ofstream outFile;
@@ -32,58 +32,59 @@ int main() {
             // (a). usar random "reales" con el tiempo como seed.
     // [3]. Calcular el valor máximo de cada secuencia y guardar cada máximo con su secuencia en otro archivo.
 
-    outFile.open("test.txt");
+    cout << "-----------This program will produce a random amount of numbers-----------" << endl;
+    cout << "Enter the amount of rows you would like to have: ";
+    cin >> rowsLimit;
+    while(rowsLimit < 1)
+    {
+        cout << "You must at least enter one row, please try again!" << endl;
+        cout << "Enter the amount of rows you would like to have: ";
+        cin >> rowsLimit;
+    }
 
-    for(int i=1; i<=10; i++)
+    outFile.open("randomSequence.txt");
+//unsigned seed =time(0);
+//srand(seed);
+    srand(time(NULL));
+    for(int i=1; i<=rowsLimit; i++)
     {
 
-        for(int i=1; i<=10; i++, mod++)
+        for(int i=1; i<=rowsLimit; i++)
         {
-            srand(time(NULL));
-            outFile << 1 + (rand()%mod);
+            outFile << 1 +(rand()%mod);
             outFile << " ";
-            cout << 1 + (rand()%mod);
-            cout << " ";
         }
-
         outFile << endl;
-        cout << endl;
     }
     outFile.close();
 
-    inFile.open("test.txt");
-    outFile.open("sequenceSUM.txt");
-    for(int i=1; i<=10; i++, row++)
+    inFile.open("randomSequence.txt");
+    outFile.open("rowsSum.txt");
+    for(int i=1; i<=rowsLimit; i++, row++)
     {
         inFile >> one >> two >> three >> four >> five >> six >> seven >> eight >> nine >> ten;
         sum = one + two + three + four + five + six + seven + eight + nine + ten;
-        cout << "Row " << row << ": " << one << two << three << four << five << six << seven << eight << nine << ten << endl;
-        outFile << "In row " << row << " the sum is: " << sum << endl;
-
-        //cout << " <-- the greatest value" << endl;
+        cout << one << " " << two << " " << three << " " << four << " " << five << " "
+             << six << " " << seven << " " << eight << " " << nine << " " << ten << endl;
+        outFile << sum << endl;
     }
 
-    //outFile << one << two << three << four << five << six << seven << eight << nine << ten;
-    //cout << one << two << three << four << five << six << seven << eight << nine << ten;
     inFile.close();
     outFile.close();
 
-    inFile.open("sequenceSUM.txt");
-    inFile >> one >> two >> three >> four >> five >> six >> seven >> eight >> nine >> ten;
-    cout << one << two << three << four << five << six << seven << eight << nine << ten;
+    inFile.open("rowsSum.txt");
 
-    if(one > two && one > three && one > four && one > five && one > six && one > seven && one > eight && one > nine && one > ten) max = one;
-    else if(two > one && two > three && two > four && two > five && two > six && two > seven && two > eight && two > nine && two > ten) max = two;
-    /*else if(three > one && three two three four three five three six  three seven three eight three nine three ten) max = three;
-    else if(four > one && four two four three four five four six four seven four eight four nine four ten) max = four;
-    else if(five > one && five two five three five four five six five seven five eight five nine five ten) max = five;
-    else if(six > one && six two six three six four six five six seven six eight six nine six ten) max = six;
-    else if(seven > one && seven two seven three seven four seven five seven six seven eight seven nine seven ten) max = seven;
-    else if(eight > one && eight two eight three eight four eight five eight six eight seven eight nine eight ten) max = eight;
-    else if(nine > one && nine two nine three nine four nine five nine six nine seven nine eight nine ten) max = nine;
-    */else if (ten > one && ten > two && ten > three && ten > four && ten > five && ten > six && ten > seven && ten > eight && ten > nine) max = ten;
+    int A = -1;
+    int B = 0;
 
-    cout << "The Max number is: " << max;
+    while(!inFile.eof())
+    {
+        inFile >> B;
+        if(A < B)
+            A=B;
+    }
+    cout << "\nThe Max number based on the sum per rows is: " << A;
+
     outFile.close();
     inFile.close();
     return 0;
