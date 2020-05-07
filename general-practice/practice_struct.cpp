4fbc73f91b5eb8663553e2a0120a4c2d4a3ae1b2
curet <jcscuret@gmail.com>
2020-05-07 16:29:37 -0400

    practicing with struct's members

--- general-practice/practice_struct.cpp
+++ general-practice/practice_struct.cpp
@@ -4,40 +4,55 @@
 
 using namespace std;
 
-struct Random{
+struct Sequences{
 
     int numberOfSequences;
+    int coordinates;
     double data;
     double standardDeviation;
     double mean;
-    double click;
+    double clicks;
     double maximum;
+    double minimum;
 
 };
 
-void testVectorStructs(vector <Random> r, vector<vector<Random> > normal){
+enum DISPLAY{TEST = 1, SUM = 2} USER; // to test enum
 
-    r.push_back(Random());                  // push_back();
+void testVectorStructs(vector <Sequences> &r, vector<vector<Sequences> > &m){
+
+    r.push_back(Sequences());                  // push_back();
     r[0].mean = 30;
     cout << r[0].mean << endl;
 
-    r.push_back(Random());                  // push_back();
+    r.push_back(Sequences());                  // push_back();
     r[0].standardDeviation = 3.8;
     cout << r[0].standardDeviation << endl;
 
-    normal.push_back(vector<Random>());     // push_back();
-    normal[0].push_back(Random());          // push_back();
-    normal[0][0].data = 0.784365;
-    cout << normal[0][0].data << endl;
+    m.push_back(vector<Sequences>());          // push_back();
+    m[0].push_back(Sequences());               // push_back();
+    m[0][0].data = 0.784365;
+    cout << m[0][0].data << endl;
 
 }
 
+void sumMeanDataSD(vector <Sequences> &r, vector<vector<Sequences> > &m){
+    cout << r[0].mean + r[0].standardDeviation + m[0][0].data;
+}
 
 int main(){
 
-    vector <Random> r;
-    vector<vector<Random> > normal;
-    testVectorStructs(r,normal);
+    int choice;
+    
+    vector <Sequences> r;
+    vector<vector<Sequences> > m;
+
+    cout << "\nDo you want to test or to sum? ";
+    cout << "\n\n[1]. testVectorStructs(r,m);";
+    cout << "\n[2]. sumMeanDataSD(r,m);\n";
+    cin >> choice;
+    if(choice == 1) testVectorStructs(r,m);
+    else {testVectorStructs(r,m); sumMeanDataSD(r,m);}
 
     return 0;
 }
