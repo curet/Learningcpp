be508b3eecad5997f0aa90cd1205bda165b3b902
curet <jcscuret@gmail.com>
2020-05-08 23:11:29 -0400

    testing vectors in loops

--- general-practice/practice_struct.cpp
+++ general-practice/practice_struct.cpp
@@ -23,21 +23,43 @@ void testVectorStructs(vector <Sequences> &r, vector<vector<Sequences> > &m){
 
     r.push_back(Sequences());                  // push_back();
     r[0].mean = 30;
-    cout << r[0].mean << endl;
+    //cout << r[0].mean << endl;
 
     r.push_back(Sequences());                  // push_back();
     r[0].standardDeviation = 3.8;
-    cout << r[0].standardDeviation << endl;
+    //cout << r[0].standardDeviation << endl;
 
     m.push_back(vector<Sequences>());          // push_back();
     m[0].push_back(Sequences());               // push_back();
     m[0][0].data = 0.784365;
-    cout << m[0][0].data << endl;
+    
+
+    m.push_back(vector<Sequences>());          // push_back();
+    m[0].push_back(Sequences());               // push_back();
+    m[0][1].data = 0.6793425;
+
+    // cout << m[0][0].data << endl;
+    // cout << m[0][1].data << endl;
+
+    //display using loops
+    cout << endl;
+    
+    for(int i=0; i<r.size()-1; i++){
+        cout <<r[0].mean << "m" << endl;
+    }
+
+    for(int i=0; i<r.size()-1; i++){
+        cout <<r[0].standardDeviation << "sd" << endl;
+    }
+
+    for(int i=0; i<m.size();i++){
+        cout << m[0][i].data << endl;
+    }
 
 }
 
 void sumMeanDataSD(vector <Sequences> &r, vector<vector<Sequences> > &m){
-    cout << r[0].mean + r[0].standardDeviation + m[0][0].data;
+    cout << r[0].mean + r[0].standardDeviation + m[0][0].data << endl;
 }
 
 int main(){
