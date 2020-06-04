4da17ee4daec187898b0ca2a5a988acd800c247b
José C.S. Curet <45988719+curet@users.noreply.github.com>
2020-06-03 23:30:12 -0400

    additional dynamic array and del. allocated memory

--- general-practice/increaseArraySizeDynamically_pointers.cpp
+++ general-practice/increaseArraySizeDynamically_pointers.cpp
@@ -7,8 +7,8 @@ int main(){
 
     //int array[] = {};
 
-    int * array = nullptr;
-    array = new int[1];
+    double * array = nullptr;
+    array = new double[1];
 
 
     cout << "Enter first values: ";
@@ -23,9 +23,38 @@ int main(){
         i++;
     }
 
-    for(int j=0; j<(i-1); j++){
+    int sizeOfArray = i-1;
+    for(int j=0; j<sizeOfArray; j++){
         cout << array[j] << " ";
     }
-    
+
+        double maximum;
+    for(int m=0; m<sizeOfArray; m++){
+
+        // find maximum value
+        if(array[m] < array[m+1]){
+            maximum = array[m+1];
+        }
+    }
+
+    cout << "\nThe maximum value is: " << maximum;
+
+        double * normalized = nullptr;
+        normalized = new double[1];
+
+    for(int k=0; k<sizeOfArray; k++){
+        normalized[k] = array[k] / maximum;
+    }
+
+        cout << endl;
+    for(int k=0; k<sizeOfArray; k++){
+
+        cout << normalized[k] << " ";
+    }
+
+    delete [] array;
+    delete [] normalized;
+
     return 0;
+    
 }
